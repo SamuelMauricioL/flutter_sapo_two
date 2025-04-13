0634677d567ba15a3c826247c51ea320c6fc3ac3
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2025-04-12 21:35:17 -0500

    fix: test

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter_sapo_two/app/app.dart';
-import 'package:flutter_sapo_two/counter/counter.dart';
+import 'package:flutter_sapo_two/posts/view/posts_view.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(PostsView), findsOneWidget);
     });
   });
 }
